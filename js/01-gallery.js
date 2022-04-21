7ecad3a0e5d8509843397abd6ee339ed00971a6f
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-04-21 21:19:37 +0300

    create gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const cardsMarkup = createGalleryCardMarkup(galleryItems)
+gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createGalleryCardMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join("");
+
+    console.log(markup);
+
+}
