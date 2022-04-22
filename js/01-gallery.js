38e31003dbd13a032f40e65d9aede8febe8b3a09
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-04-22 23:45:03 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector(".gallery")
+
 const cardsMarkup = createGalleryCardMarkup(galleryItems)
 gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+gallery.addEventListener('click', onGalleryElClick)
 
 function createGalleryCardMarkup(items) {
     return items.map(({ preview, original, description }) => {
         return `
     <div class="gallery__item">
-        <a class="gallery__link" href="large-image.jpg">
+        <a class="gallery__link" href=#">
             <img
                 class="gallery__image"
                 src="${preview}"
@@ -20,7 +22,24 @@ function createGalleryCardMarkup(items) {
     </div>
     `;
     }).join("");
+}
 
-    console.log(markup);
+function onGalleryElClick(e) {
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const originalImg = e.target.dataset.source;
+    modalWindow(originalImg);
+}
 
+function modalWindow(img) {
+    const instance = basicLightbox.create(`
+    <img src="${img}" width="800" height="600">
+`)
+    instance.show()
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+            instance.close()
+        }
+    })
 }
