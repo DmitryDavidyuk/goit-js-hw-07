89cadacb276c85d3c20fac594292691f75d0fa6d
DmitryDavidyuk <dima.demon.95@gmail.com>
2022-04-26 19:15:43 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+const cardsMarkup = createGalleryCardMarkup(galleryItems)
+gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createGalleryCardMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    }).join("");
+}
